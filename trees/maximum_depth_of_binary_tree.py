43ee2ffa40e1ee4e59fd0a5fdd87c5161706aeb7
stefan-500 <wqygstefan900zwqi@gmail.com>
2026-05-13 09:41:09 +0200

    feat: maximum depth of binary tree
    Add solution for 'maximum depth of binary tree'

---
+++ trees/maximum_depth_of_binary_tree.py
@@ -0,0 +1,54 @@
+from typing import Optional
+from collections import deque
+
+# Definition for a binary tree node.
+class TreeNode:
+  def __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+"""
+Run in the neetcode text editor.
+"""
+class Solution:
+
+  # Breadth First Search solution | Time: O(n), Space: O(n)
+  # def maxDepth(self, root: Optional[TreeNode]) -> int:
+    # q = deque([root])
+    # if root:
+    #   q.append(root)      
+
+    # level = 0
+    # while q:
+    #   for i in range(len(q)):
+    #     node = q.popleft()
+    #     if node.left:
+    #       q.append(node.left)
+    #     if node.right:
+    #       q.append(node.right)
+    #   level += 1
+    # return level
+
+
+  # Iterative DFS solution | Time: O(n), Space: O(n)
+  # def maxDepth(self, root: Optional[TreeNode]) -> int:
+  #   stack = [[root, 1]]
+  #   res = 0
+
+  #   while stack:
+  #     node, depth = stack.pop()
+      
+  #     if node:
+  #       res = max(res, depth)
+  #       stack.append([node.left, depth + 1])
+  #       stack.append([node.right, depth + 1])
+  #   return res
+
+
+  # Recursive DFS solution (best) | Time: O(n), Space: O(n)
+  def maxDepth(self, root: Optional[TreeNode]) -> int:
+    if not root:
+      return 0
+    
+    return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
