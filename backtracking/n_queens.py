2926c5ae77a315591bec64d890d150753580bb55
stefan-500 <wqygstefan900zwqi@gmail.com>
2026-07-12 21:01:29 +0200

    feat: n queens
    Add solution for 'n queens'

---
+++ backtracking/n_queens.py
@@ -0,0 +1,40 @@
+from typing import List
+
+"""
+Run in the neetcode text editor.
+"""
+class Solution:
+
+  # Backtracking + Hash Set solution (best) | Time: O(n!), Space: O(n^2)
+  def solveNQueens(self, n: int) -> List[List[str]]:
+    col = set()
+    posDiag = set()
+    negDiag = set()
+
+    res = []
+    board = [["."] * n for i in range(n)]
+
+    def backtrack(r):
+      if r == n:
+        copy = ["".join(row) for row in board]
+        res.append(copy)
+        return
+
+      for c in range(n):
+        if c in col or (r + c) in posDiag or (r - c) in negDiag:
+          continue
+        
+        col.add(c)
+        posDiag.add(r + c)
+        negDiag.add(r - c)
+        board[r][c] = "Q"
+
+        backtrack(r + 1)
+
+        col.remove(c)
+        posDiag.remove(r + c)
+        negDiag.remove(r - c)
+        board[r][c] = '.'
+
+    backtrack(0)
+    return res
