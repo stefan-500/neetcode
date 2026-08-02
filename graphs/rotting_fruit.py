b726f8fb89f6ffd6ad6ac7f97788870bb245c628
stefan-500 <wqygstefan900zwqi@gmail.com>
2026-08-02 19:41:23 +0200

    feat: rotten fruit
    Add solution for 'rotten fruit'

---
+++ graphs/rotting_fruit.py
@@ -0,0 +1,38 @@
+from typing import List
+from collections import deque
+
+"""
+Run in the neetcode text editor.
+"""
+class Solution:
+
+  # Multi source BFS solution (best) | Time: O(m * n), Space: O(m * n),
+  # where m is the number of rows and n is the number of columns in the grid.
+  def orangesRotting(self, grid: List[List[int]]) -> int:
+    q = deque()
+    fresh = 0
+    time = 0
+
+    for r in range(len(grid)):
+      for c in range(len(grid[0])):
+        if grid[r][c] == 1:
+          fresh += 1
+        if grid[r][c] == 2:
+          q.append((r, c))
+    
+    directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+    while fresh > 0 and q:
+      length = len(q)
+      for i in range(length):
+        r, c = q.popleft()
+
+        for dr, dc in directions:
+          row, col = r + dr, c + dc
+          if (row in range(len(grid)) and col in range(len(grid[0]))
+          and grid[row][col] == 1):
+            grid[row][col] = 2
+            q.append((row, col))
+            fresh -= 1
+        
+      time += 1
+    return time if fresh == 0 else -1
